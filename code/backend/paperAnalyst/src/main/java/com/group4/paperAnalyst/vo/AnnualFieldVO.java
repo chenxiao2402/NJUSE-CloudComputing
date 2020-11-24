4c12300082d989b442510bfc8d36e9ecc3ec20da
Junda Liao <284983325@qq.com>
2020-11-24 17:05:57 +0800

    Modified api PopularAnnualField

---
+++ code/backend/paperAnalyst/src/main/java/com/group4/paperAnalyst/vo/AnnualFieldVO.java
@@ -0,0 +1,40 @@
+package com.group4.paperAnalyst.vo;
+
+public class AnnualFieldVO {
+
+    private Long year;
+
+    private String field;
+
+    private Long count;
+
+    public AnnualFieldVO(Long year, String field, Long count) {
+        this.year = year;
+        this.field = field;
+        this.count = count;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public Long getYear() {
+        return year;
+    }
+
+    public void setYear(Long year) {
+        this.year = year;
+    }
+}
