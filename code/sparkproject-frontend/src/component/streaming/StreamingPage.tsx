0fb1567f63c2106061a49036f3450bafa61dbed6
Sephidator <Sephidator@gmail.com>
2020-11-24 13:39:47 +0800

    .

--- code/sparkproject-frontend/src/component/streaming/StreamingPage.tsx
+++ code/sparkproject-frontend/src/component/streaming/StreamingPage.tsx
@@ -5,6 +5,7 @@ import { LoadingOutlined, UploadOutlined } from '@ant-design/icons';
 import {RadioChangeEvent} from 'antd/lib/radio';
 import ROUTES from '../../utilities/routes';
 import {URL, sendRequest} from "../../utilities/axios";
+import echarts from "../../utilities/echarts";
 
 const PopularFields = lazy( () => import('./PopularFields'));
 const DynamicRanking = lazy(() => import('./PopularFieldRanking'));
@@ -17,7 +18,8 @@ interface IState {
     loadingStream: boolean,
     showLoadingPage: boolean,
     dataNumber: number,
-    dataStableDuration: number
+    dataStableDuration: number,
+    streamingData: Array<any>
 }
 
 class StreamingPage extends Component<any, IState> {
@@ -26,9 +28,10 @@ class StreamingPage extends Component<any, IState> {
         super(props);
         this.state = {
             loadingStream: false,
-            showLoadingPage: Boolean(localStorage.getItem('streamLoaded')),
+            showLoadingPage: !Boolean(localStorage.getItem('streamLoaded')),
             dataNumber: 0,
-            dataStableDuration: 0
+            dataStableDuration: 0,
+            streamingData: []
         };
     }
 
@@ -41,19 +44,78 @@ class StreamingPage extends Component<any, IState> {
                 if (localStorage.getItem('streamLoaded')) {
                     clearInterval()
                 } else {
-                    sendRequest(URL.ALL_POPULAR_FIELD_RANKING, {}, (data) => {
+                    sendRequest(URL.YEAR_PAPER_COUNT, {}, (data) => {
                         const dataStableDuration = this.state.dataStableDuration + (this.state.dataNumber === data.dataNumber ? 1 : 0);
                         if (dataStableDuration >= 3) {
                             localStorage.setItem('streamLoaded', 'true');
                         }
                         this.setState({
+                            streamingData: data,
                             showLoadingPage: Boolean(localStorage.getItem('streamLoaded'))
-                        })
+                        });
+                        this.setBarChart();
                     })
                 }
             }, 1000);
         });
     };
+
+    setBarChart = () => {
+        if (this.state.showLoadingPage) {
+            var myChart = echarts.init(document.getElementById('streamingChart'));
+            alert();
+            myChart.setOption({
+                color: ['#3398DB'],
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                        type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                    }
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis: [
+                    {
+                        type: 'category',
+                        data: this.state.streamingData.map(e => e.year),
+                        axisTick: {
+                            alignWithLabel: true
+                        }
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value'
+                    }
+                ],
+                series: [
+                    {
+                        name: '已读取数据量',
+                        type: 'bar',
+                        barWidth: '60%',
+                        data: this.state.streamingData.map(e => e.count)
+                    }
+                ]
+            });
+        }
+    };
+
+    componentDidMount(): void {
+        sendRequest(URL.YEAR_PAPER_COUNT, {}, (data) => {
+            this.setState({
+                streamingData: data
+            });
+            this.setBarChart();
+        })
+    }
+
+    componentDidUpdate(): void {
+
+    }
 
     render() {
         return (
@@ -63,7 +125,7 @@ class StreamingPage extends Component<any, IState> {
                         <span style={{width: 180, display: 'inline-block', textAlign: 'left'}}>流读取论文数：{this.state.dataNumber}</span>
                         {this.state.loadingStream ? <LoadingOutlined /> : <Button shape='circle' icon={<UploadOutlined/>} onClick={this.loadStream} />}
                     </div>
-
+                    <div id={'streamingChart'} style={{width: '0.6ww', height: 700, marginTop: 64}} />
                 </div> :
                 <div>
                     <Radio.Group defaultValue={window.location.pathname} buttonStyle='solid' onChange={onChange}
